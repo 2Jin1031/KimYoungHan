9ee1e5a129f0009192ca7ef3130a6e79f077cc97
2jin2.1031 <2jin2.1031@gmail.com>
2024-03-11 15:40:21 +0900

    Feat: Jpa #15 (6-5)

--- hello-springParrot/src/test/java/hello/hellospring/service/MemberServiceIntergrationTest.java
+++ hello-springParrot/src/test/java/hello/hellospring/service/MemberServiceIntergrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -20,6 +21,7 @@ class MemberServiceIntergrationTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
+    @Commit
     void 회원가입() {
 
         // given
