3e975957562cded45c4b6abd66de6463b6eeaea0
2jin2.1031 <2jin2.1031@gmail.com>
2024-03-20 17:14:07 +0900

    Feat: JPA and DB settings, operation check #18 (1-5)

---
+++ jpashopParrot/src/test/java/jpabook/jpashopParrot/MemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package jpabook.jpashopParrot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void memberTest(){
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+        memberRepository.save(member);
+
+        //when
+        Long savedId = member.getId();
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        Assertions.assertThat(member.getId()).isEqualTo(findMember.getId());
+        Assertions.assertThat(member.getUsername()).isEqualTo(findMember.getUsername());
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
