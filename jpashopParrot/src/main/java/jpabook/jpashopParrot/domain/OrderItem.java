ed45b8ef2eea31cf764fcdb6d887e9b7d60753ec
2jin2.1031 <2jin2.1031@gmail.com>
2024-03-26 17:41:24 +0900

    chore: add comments #23

--- jpashopParrot/src/main/java/jpabook/jpashopParrot/domain/OrderItem.java
+++ jpashopParrot/src/main/java/jpabook/jpashopParrot/domain/OrderItem.java
@@ -46,6 +46,8 @@ public class OrderItem {
         getItem().addStock(count);
     }
 
+    //==조회 로직==//
+    //주문상품 전체 가격 조회
     public int getTotalPrice() {
         return getOrderPrice() * getCount();
     }
