fa79b8cc007703e7a72ef041a17ff1914a52cfb1
2jin2.1031 <2jin2.1031@gmail.com>
2024-03-30 17:58:15 +0900

    Feat: Hello JPA - Application Development #26 (2-2)

--- ex1-hello-jpaParrot/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpaParrot/src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,39 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("helloJPA");
+//            em.persist(member);
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(8)
+                            .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.getName( = " + member.getName());
+            }
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
