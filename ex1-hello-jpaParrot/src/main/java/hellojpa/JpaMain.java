6de424995ca3db042da52ced126ff611d7bc6679
2jin2.1031 <2jin2.1031@gmail.com>
2024-04-01 18:09:41 +0900

    Feat: Master of two-way associations and associations 1- Basics #29 (5-2)

--- ex1-hello-jpaParrot/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpaParrot/src/main/java/hellojpa/JpaMain.java
@@ -25,6 +25,17 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m : members) {
+                System.out.println("m = " + m.getName());
+            }
+
             tx.commit();
         } catch (Exception e) {
             System.out.println("in catch");
