1ef93b7201fb94b6205b63b32a96f28643c5390a
2jin2.1031 <2jin2.1031@gmail.com>
2024-04-01 17:40:20 +0900

    Feat: one-way association #29 (5-1)

--- ex1-hello-jpaParrot/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpaParrot/src/main/java/hellojpa/JpaMain.java
@@ -1,34 +1,38 @@
-//package hellojpa;
-//
-//import jakarta.persistence.*;
-//
-//import java.util.List;
-//
-//public class JpaMain {
-//
-//    public static void main(String[] args) {
-//
-//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-//        EntityManager em = emf.createEntityManager();
-//
-//        EntityTransaction tx = em.getTransaction();
-//        tx.begin();
-//
-//        try {
-//            // 비영속
-//            Member findMember = em.find(Member.class, 150L);
-//            System.out.println("findMember = " + findMember);
-//            findMember.setName("ZZZZZZZ");
-//
-//            System.out.println("=========");
-//            tx.commit();
-//        } catch (Exception e) {
-//            System.out.println("in catch");
-//            tx.rollback();
-//        } finally {
-//            em.close();
-//        }
-//
-//        emf.close();
-//    }
-//}
+package hellojpa;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+
+            Member member = new Member();
+            member.setName("memberA");
+            member.setTeam(team);
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println("in catch");
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
--- ex1-hello-jpaParrot/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpaParrot/src/main/java/hellojpa/Member.java
@@ -1,36 +1,53 @@
-//package hellojpa;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Id;
-//
-//@Entity
-//public class Member {
-//
-//    @Id
-//    private Long id;
-//    private String name;
-//
-//    public Member() {
-//    }
-//
-//    public Member(Long id, String name) {
-//        this.id = id;
-//        this.name = name;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//}
+package hellojpa;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String name;
+
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Member() {
+    }
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+}
