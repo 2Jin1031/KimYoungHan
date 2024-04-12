fe0d76aa86be71c6a16eb13b3db08447baaf5610
2jin2.1031 <2jin2.1031@gmail.com>
2024-04-12 15:29:55 +0900

    refactor: 메소드 위치 일관성을 위한 변경 #30

--- ex1-hello-jpaParrot/src/main/java/hellojpa/jpabook/jpashop/domain/Item.java
+++ ex1-hello-jpaParrot/src/main/java/hellojpa/jpabook/jpashop/domain/Item.java
@@ -17,12 +17,12 @@ public class Item {
     private int price;
     private int stockQuantity;
 
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
     public Long getId() {
         return id;
     }
-
-    @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<>();
 
     public void setId(Long id) {
         this.id = id;
